7d0b24a17c2fe6f4d01957fe209621711a7ccc68
chippybautista <chippybautista@gmail.com>
2019-08-26 22:13:07 +0800

    add normalizer

--- data.py
+++ data.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
-from sklearn.preprocessing import LabelEncoder, minmax_scale
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 
 from settings import *
@@ -15,9 +15,9 @@ class BOLD5000:
         df = _df[~_df[level].isnull()]
 
         # hardcoding downsampling ugh...
-        # red_rows = df[df[level] == 'red'].index.values
-        # rows_to_drop = np.random.choice(red_rows, 2200, replace=False)
-        # df = df.drop(rows_to_drop)
+        red_rows = df[df[level] == 'red'].index.values
+        rows_to_drop = np.random.choice(red_rows, 2200, replace=False)
+        df = df.drop(rows_to_drop)
 
         self.slice_filenames = df['slice_filename'].values
         self.stimulus_filenames = df['stimulus_filename'].values
@@ -31,22 +31,37 @@ class BOLD5000:
             len(self.labels), len(_df)))
         print(df[level].value_counts())
 
+    def fit_normalizer(self, train_data):
+        self.normalizer = StandardScaler(with_std=True)  # zero mean only
+        for i in range(0, len(train_data), 50):
+            filenames = train_data[i:i + 50]
+            batch = np.array([
+                np.load(SLICE_DIR + filename + '.npy')
+                for filename in filenames
+            ])
+            self.normalizer.partial_fit(batch.reshape(len(filenames), -1))
+        print('Normalizer is fit on training data.')
+
     def train_test_split(self, batch_size=32):
         def _get_dataloader(x, y):
-            return DataLoader(BOLD5000_Split(x, y), batch_size=batch_size)
+            return DataLoader(BOLD5000_Split(x, y, self.normalizer),
+                              batch_size=batch_size)
 
         X_train, X_test, y_train, y_test = train_test_split(
             self.slice_filenames, self.labels,
             train_size=0.8, stratify=self.labels)
+
+        self.fit_normalizer(X_train)
 
         return (_get_dataloader(X_train, y_train),
                 _get_dataloader(X_test, y_test))
 
 
 class BOLD5000_Split(Dataset):
-    def __init__(self, slice_filenames, labels):
+    def __init__(self, slice_filenames, labels, normalizer):
         self.slice_filenames = slice_filenames
         self.labels = labels
+        self.normalizer = normalizer
 
     def __len__(self):
         return len(self.slice_filenames)
@@ -58,7 +73,8 @@ class BOLD5000_Split(Dataset):
         # )
         fmri_slice = np.load(SLICE_DIR + self.slice_filenames[i] + '.npy')
         return (
-            minmax_scale(fmri_slice.reshape(-1, 1), (-1, 1)
-                         ).reshape(fmri_slice.shape),
+            self.normalizer.transform(
+                fmri_slice.reshape(1, -1)).reshape(fmri_slice.shape),
             self.labels[i]
+
         )
